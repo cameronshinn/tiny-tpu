fa3259079f941cfd85418bfb0e570f403b13b1c9
Swasly <watonks@ucdavis.edu>
2019-03-27 17:47:52 -0700

    [MMU] Completed sysArr.v; can be compiled (not verified yet)

--- hardware/hdl/mmu/sysArr.v
+++ hardware/hdl/mmu/sysArr.v
@@ -38,68 +38,73 @@ module sysArr(
     output [data_width-1:0] dataout; // 8 bits for each row. Top row has LSB
 
     // Interconnects (Row - Row Connections)
+    wire [((width_height-1)*width_height*16)-1:0] maccout_inter;
+    wire [((width_height-1)*width_height*8)-1:0] wout_inter;
+    wire [((width_height-1)*width_height)-1:0] wwriteout_inter;
+    wire [((width_height-1)*width_height)-1:0] activeout_inter;
 
-    genvar j;
+    genvar i;
     generate
-        for (j = 0; j < width_height; j = j + 1) begin
-            if (j == 0) begin
+        for (i = 0; i < width_height; i = i + 1) begin
+            if (i == 0) begin
                 // The first row has different inputs
                 sysArrRow first_sysArrRow_inst(
-                    .clk      (),
-                    .active   (),
-                    .datain   (),
-                    .win      (),
-                    .sumin    (),
-                    .wwrite   (),
-                    .maccout  (),
-                    .wout     (),
-                    .wwriteout(),
-                    .activeout(),
-                    .dataout  ()
+                    .clk      (clk),
+                    .active   (active),
+                    .datain   (datain[((i+1)*8)-1:(i*8)]),
+                    .win      (win),
+                    .sumin    (0),
+                    .wwrite   (wwrite),
+                    .maccout  (maccout_inter[((i+1)*width_height*16)-1:(i*width_height*16)]),
+                    .wout     (wout_inter[((i+1)*width_height*8)-1:(i*width_height*8)]),
+                    .wwriteout(wwriteout_inter[((i+1)*width_height)-1:(i*width_height)]),
+                    .activeout(activeout_inter[((i+1)*width_height)-1:(i*width_height)]),
+                    .dataout  (dataout[((i+1)*8)-1:(i*8)])
                 );
 
                 defparam first_sysArrRow_inst.row_width = width_height;
 
-            end // if (j == 0)
+            end // if (i == 0)
 
-            else if (j == width_height-1) begin
+            else if (i == width_height-1) begin
                 // The last row has different outputs
-                sysArrRow sysArrRow_inst(
-                    .clk      (),
-                    .active   (),
-                    .datain   (),
-                    .win      (),
-                    .sumin    (),
-                    .wwrite   (),
-                    .maccout  (),
-                    .wout     (),
-                    .wwriteout(),
-                    .activeout(),
-                    .dataout  ()
+                sysArrRow last_sysArrRow_inst(
+                    .clk      (clk),
+                    .active   (activeout_inter[(i*width_height)-1:((i-1)*width_height)]),
+                    .datain   (datain[((i+1)*8)-1:(i*8)]),
+                    .win      (wout_inter[(i*width_height*8)-1:((i-1)*width_height*8)]),
+                    .sumin    (maccout_inter[(i*width_height*16)-1:((i-1)*width_height*16)]),
+                    .wwrite   (wwriteout_inter[(i*width_height)-1:((i-1)*width_height)]),
+                    .maccout  (maccout),
+                    .wout     (wout),
+                    .wwriteout(wwriteout),
+                    .activeout(activeout),
+                    .dataout  (dataout[((i+1)*8)-1:(i*8)])
                 );
 
                 defparam sysArrRow_inst.row_width = width_height;
 
-            end // else if (j == width_height-1)
+            end // else if (i == width_height-1)
 
             else begin
                 // intermediate rows have generic inputs/outputs
-                sysArrRow last_sysArrRow_inst(
-                    .clk      (),
-                    .active   (),
-                    .datain   (),
-                    .win      (),
-                    .sumin    (),
-                    .wwrite   (),
-                    .maccout  (),
-                    .wout     (),
-                    .wwriteout(),
-                    .activeout(),
-                    .dataout  ()
+                sysArrRow sysArrRow_inst(
+                    .clk      (clk),
+                    .active   (activeout_inter[(i*width_height)-1:((i-1)*width_height)]),
+                    .datain   (datain[((i+1)*8)-1:(i*8)]),
+                    .win      (wout_inter[(i*width_height*8)-1:((i-1)*width_height*8)]),
+                    .sumin    (maccout_inter[(i*width_height*16)-1:((i-1)*width_height*16)]),
+                    .wwrite   (wwriteout_inter[(i*width_height)-1:((i-1)*width_height)]),
+                    .maccout  (maccout_inter[((i+1)*width_height*16)-1:(i*width_height*16)]),
+                    .wout     (wout_inter[((i+1)*width_height*8)-1:(i*width_height*8)]),
+                    .wwriteout(wwriteout_inter[((i+1)*width_height)-1:(i*width_height)]),
+                    .activeout(activeout_inter[((i+1)*width_height)-1:(i*width_height)]),
+                    .dataout  (dataout[((i+1)*8)-1:(i*8)])
                 );
 
                 defparam last_sysArrRow_inst.row_width = width_height;
 
             end // else
-        end // for (j = 0; j < width_height; j = j + 1)
+        end // for (i = 0; i < width_height; i = i + 1)
     endgenerate
+endmodule // sysArr
