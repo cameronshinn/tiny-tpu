0ca764c12e4ceb6f9be6afbf19cdf5d65d111455
SkyrimYu <30339416+SkyrimYu@users.noreply.github.com>
2019-04-26 20:39:44 -0700

    Update fifo_control.v

--- hardware/hdl/control/fifo_control.v
+++ hardware/hdl/control/fifo_control.v
@@ -4,36 +4,53 @@
 // Apri 22 2019
 module fifo_control(
   clk,
-  reset, // reset the program, high active
-  active, // enable the modulel, high active 
+  reset,
+  active,
   fifo_en
   );
 
-  parameter fifo_width = 4; // the number of fifo width
+  parameter fifo_width = 4;
 
   input clk, reset, active;
   output reg [fifo_width-1:0] fifo_en;
   reg [fifo_width-1:0] fifo_en_c;
+  reg fifo_dec; // enable starts to decrease
+  reg state, state_c;
+
+  parameter Hold = 1'b0;
+  parameter Start = 1'b1;
 
   always@(posedge clk) begin
     fifo_en <= fifo_en_c;
+    state <= state_c;
   end
 
   always@(*) begin
-    if(active) begin
-      fifo_en_c = (fifo_en << 1) + 1;
-
-      if(fifo_en = 4'b1111) begin
-        fifo_en_c = 4'b1111;
+    case (state)
+      Hold: begin
+        if(active) begin
+          state_c = Start;
+        end
       end
-    end
 
-    else begin
-      fifo_en_c = 4'b0000; // if not active, no fifo_en is active.
-    end
+      Start: begin
+        if(fifo_en == 4'b1111) begin
+          fifo_dec = 1;
+        end
+
+        if(fifo_dec) begin
+          fifo_en_c = fifo_en << 1;
+        end
+
+        else begin
+          fifo_en_c = (fifo_en << 1) + 1;
+        end
+      end
+      endcase
 
     if(reset) begin
       fifo_en_c = 4'b0000;
+      state_c = Hold;
     end
   end
 endmodule
