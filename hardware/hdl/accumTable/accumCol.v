071cdc84bff89f33adc45ed96d20b058d4257f72
cameronshinn <cameron.shinn@hotmail.com>
2019-05-03 01:47:06 -0700

    Completeed (untested) accumulator table

--- hardware/hdl/accumTable/accumCol.v
+++ hardware/hdl/accumTable/accumCol.v
@@ -1,68 +1,43 @@
 // accumCol.v
 // Cameron Shinn
 
-// Inputs:
-//
-//
-
-// Outputs:
-//
-//
-
-module accumCol(clk, reset, rd_addr, rd_data, wr_addr, wr_data, wr_en);
+module accumCol(clk, reset, rd_en, wr_en, rd_addr, wr_addr, rd_data, wr_data);
 
     parameter DATA_WIDTH = 8; // number of bits for one piece of data
     parameter MAX_OUT_ROWS = 128; // output height of largest matrix
     parameter MAX_OUT_COLS = 128; // output width of largest possible matrix
-    parameter SYS_ARR_WIDTH = 16; // height of the systolic array
+    parameter SYS_ARR_COLS = 16; // height of the systolic array
 
-    localparam NUM_ACCUM_ROWS = MAX_OUT_ROWS * $ceil(MAX_OUT_COLS/SYS_ARR_WIDTH);
+    localparam NUM_ACCUM_ROWS = MAX_OUT_ROWS * (MAX_OUT_COLS/SYS_ARR_COLS);
 
     input clk;
-    input reset; // sets all entries in column to 0
-    input [$clog2(NUM_ACCUM_ROWS)-1:0] rd_addr, wr_addr; // compute the necessary address space
+    input reset; // clears array and sets it to 0
+    input rd_en; // reads the data at rd_addr when high
+    input wr_en; // writes (and accumulates) data to wr_addr when high
+    input [$clog2(NUM_ACCUM_ROWS)-1:0] rd_addr;
+    input [$clog2(NUM_ACCUM_ROWS)-1:0] wr_addr;
+    output reg [DATA_WIDTH-1:0] rd_data;
     input [DATA_WIDTH-1:0] wr_data;
-    input wr_en;
-    output [DATA_WIDTH-1:0] rd_data;
 
-    wire [NUM_ACCUM_ROWS-1:0] en;
-    wire [NUM_ACCUM_ROWS*DATA_WIDTH-1:0] d, q;
+    reg [DATA_WIDTH-1:0] mem [NUM_ACCUM_ROWS-1:0];
 
-    wire [DATA_WIDTH-1:0] d_array [NUM_ACCUM_ROWS-1:0];
-    wire [DATA_WIDTH-1:0] q_array [NUM_ACCUM_ROWS-1:0];
-
-    reg [DATA_WIDTH-1:0] adder_out:
-
-    dff8 dffArray[NUM_ACCUM_ROWS-1:0] (
-        .clk(clk),
-        .reset(reset),
-        .en(en), // need to control enable so that entries don't accumulate on themselves
-        .d(d),
-        .q(q)
-    );
-
-    // translate d and q signals to an array of DATA_WIDTH-bit inputs and outputs
-    generate
-        genvar i;
-        for (i=0; i<NUM_ACCUM_ROWS; i=i+1) begin : fill_array
-            assign d_array[i] = d[DATA_WIDTH*(i+1)-1 -: DATA_WIDTH];
-            assign q_array[i] = q[DATA_WIDTH*(i+1)-1 -: DATA_WIDTH];
-        end // for (i=0; i<NUM_ACCUM_ROWS; i=i+1)
-    endgenerate
+    integer i; // used for indexing
 
     always @(posedge clk) begin
+        if (reset) begin
+            for (i = 0; i < NUM_ACCUM_ROWS; i = i + 1) begin // clear all entries to 0
+                mem[i] <= 0; // not sure if there is a good way to define literal width
+            end // for (i = 0; i < NUM_ACCUM_ROWS; i = i + 1)
+        end // if (reset)
 
-        en = 0; // reset enable signals
-        
-        if (wr_en) begin
-            en[2**wr_addr-1] = 1
-            adder_out = wr_data + q_array[wr_addr];
-            d_array[wr_addr] = adder_out;
+        else begin
+            if (wr_en) begin
+                mem[wr_addr] <= mem[wr_addr] + wr_data;
+            end // if (wr_en)
 
-        end // if (wr_en)
-
-        rd_data = q_array[rd_addr];
-
+            if (rd_en) begin
+                rd_data <= mem[rd_addr];
+            end // if (rd_en)
+        end // else
     end // always @(posedge clk)
-
 endmodule // accumCol
--- hardware/hdl/accumTable/accumTable.v
+++ hardware/hdl/accumTable/accumTable.v
@@ -5,25 +5,42 @@
 //
 // clk -- 
 // reset -- 
-// rdAddr -- 
-// wrAddr -- 
-// wrData -- 
+// rd_addr -- 
+// wr_addr -- 
+// wr_data -- 
 
 // Outputs:
 //
-// rdData -- 
+// rd_data -- 
 
-module accumTable(clk, reset, rdAddr, rdDatam, wrAddr, wrData);
+module accumTable(clk, reset, rd_en, wr_en, rd_addr, wr_addr, rd_data, wr_data);
 
-    parameter DATA_WIDTH = 8;  // number of bits for one piece of data
-    parameter MAX_OUT_ROWS = 1024;  // output number of rows in 
-    parameter MAX_OUT_COLS = 1024;
+    parameter DATA_WIDTH = 8; // number of bits for one piece of data
+    parameter MAX_OUT_ROWS = 128; // output number of rows in 
+    parameter MAX_OUT_COLS = 128;
     parameter SYS_ARR_ROWS = 16;
     parameter SYS_ARR_COLS = 16;
-    localparam IN_WIDTH
+    
+    localparam NUM_ACCUM_ROWS = MAX_OUT_ROWS * (MAX_OUT_COLS/SYS_ARR_COLS);
 
     input clk;
-    input reset;
-    input [$clog2(MAX_OUT_ROWS):0] rdAddr, wrAddr;
-    input [] wrData;
-    output [] rdData;
+    input [SYS_ARR_COLS-1:0] reset;
+    input [SYS_ARR_COLS-1:0] rd_en;
+    input [SYS_ARR_COLS-1:0] wr_en;
+    input [$clog2(NUM_ACCUM_ROWS)*SYS_ARR_COLS-1:0] rd_addr;
+    input [$clog2(NUM_ACCUM_ROWS)*SYS_ARR_COLS-1:0] wr_addr;
+    output reg [DATA_WIDTH*SYS_ARR_COLS-1:0] rd_data;
+    input [DATA_WIDTH*SYS_ARR_COLS-1:0] wr_data;
+
+    accumCol colArray [0:SYS_ARR_COLS-1] (
+        .clk(clk),
+        .reset(reset),
+        .rd_en(rd_en),
+        .wr_en(wr_en),
+        .rd_addr(rd_addr),
+        .wr_addr(wr_addr),
+        .rd_data(rd_data),
+        .wr_data(wr_data)
+    );
+
+endmodule // accumTable
