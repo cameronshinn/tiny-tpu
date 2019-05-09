334f884bf68da1816a358f80c46640377b641791
Cameron Shinn <cameron.shinn@hotmail.com>
2019-05-09 15:43:59 -0700

    Formatting

--- benchmarking/xrays/train.cpp
+++ benchmarking/xrays/train.cpp
@@ -38,16 +38,15 @@ tiny_dnn::vec_t str_to_labels(std::string label_str)
     std::stringstream str_stream(label_str);
     std::string tmp_str;
 
-    while(getline(str_stream, tmp_str, '|')) 
-    { 
-      if (classif_nums.find(tmp_str) == classif_nums.end()) {
-        std::cerr << "Find Error: Label "
-                  << tmp_str
-                  << " has no classification"
-                  << std::endl;
-      } else {
-        image_labels.push_back(classif_nums[tmp_str]);
-      }
+    while (getline(str_stream, tmp_str, '|')) { 
+        if (classif_nums.find(tmp_str) == classif_nums.end()) {
+            std::cerr << "Find Error: Label "
+                      << tmp_str
+                      << " has no classification"
+                      << std::endl;
+        } else {
+            image_labels.push_back(classif_nums[tmp_str]);
+        }
     } 
     
     std::sort(image_labels.begin(), image_labels.end());
@@ -71,6 +70,7 @@ void convert_image(const std::string& imagefilename,
 
     std::transform(resized.begin(), resized.end(), std::back_inserter(d),
                    [=](uint8_t c) { return c * scale; });
+    
     data.push_back(d);
 }
 
@@ -82,213 +82,226 @@ void parse_csv_data(std::string csv_path,
 }
 
 static void construct_net(tiny_dnn::network<tiny_dnn::sequential> &nn,
-                          tiny_dnn::core::backend_t backend_type) {
-// connection table [Y.Lecun, 1998 Table.1]
-#define O true
-#define X false
-  // clang-format off
-  // 6 rows x 16 cols?
-static const bool tbl[] = {
-    O, X, X, X, O, O, O, X, X, O, O, O, O, X, O, O,
-    O, O, X, X, X, O, O, O, X, X, O, O, O, O, X, O,
-    O, O, O, X, X, X, O, O, O, X, X, O, X, O, O, O,
-    X, O, O, O, X, X, O, O, O, O, X, X, O, X, O, O,
-    X, X, O, O, O, X, X, O, O, O, O, X, O, O, X, O,
-    X, X, X, O, O, O, X, X, O, O, O, O, X, O, O, O
-};
-// clang-format on
-#undef O
-#undef X
+                          tiny_dnn::core::backend_t backend_type)
+{
+    // connection table [Y.Lecun, 1998 Table.1]
+    #define O true
+    #define X false
+    // clang-format off
+    // 6 rows x 16 cols?
+    static const bool tbl[] = {
+        O, X, X, X, O, O, O, X, X, O, O, O, O, X, O, O,
+        O, O, X, X, X, O, O, O, X, X, O, O, O, O, X, O,
+        O, O, O, X, X, X, O, O, O, X, X, O, X, O, O, O,
+        X, O, O, O, X, X, O, O, O, O, X, X, O, X, O, O,
+        X, X, O, O, O, X, X, O, O, O, O, X, O, O, X, O,
+        X, X, X, O, O, O, X, X, O, O, O, O, X, O, O, O
+    };
+    // clang-format on
+    #undef O
+    #undef X
 
-  // construct nets
-  //
-  // C : convolution
-  // S : sub-sampling
-  // F : fully connected
-  // clang-format off
-  using fc = tiny_dnn::layers::fc;
-  using conv = tiny_dnn::layers::conv;
-  using ave_pool = tiny_dnn::layers::ave_pool;
-  using relu = tiny_dnn::activation::relu;
+    // construct nets
+    //
+    // C : convolution
+    // S : sub-sampling
+    // F : fully connected
+    // clang-format off
+    using fc = tiny_dnn::layers::fc;
+    using conv = tiny_dnn::layers::conv;
+    using ave_pool = tiny_dnn::layers::ave_pool;
+    using relu = tiny_dnn::activation::relu;
 
-  using tiny_dnn::core::connection_table;
-  using padding = tiny_dnn::padding;
+    using tiny_dnn::core::connection_table;
+    using padding = tiny_dnn::padding;
 
-  nn << conv(128, 128, 5, 1, 6,   // C1, 1@128x128-in, 6@124x124-out
-             padding::valid, true, 1, 1, 1, 1, backend_type)
-     << relu()
-     << ave_pool(28, 28, 6, 2)   // S2, 6@28x28-in, 6@14x14-out
-     << relu()
-     << conv(14, 14, 5, 6, 16,   // C3, 6@14x14-in, 16@10x10-out
-             connection_table(tbl, 6, 16),
-             padding::valid, true, 1, 1, 1, 1, backend_type)
-     << relu()
-     << ave_pool(10, 10, 16, 2)  // S4, 16@10x10-in, 16@5x5-out
-     << relu()
-     << conv(5, 5, 5, 16, 120,   // C5, 16@5x5-in, 120@1x1-out
-             padding::valid, true, 1, 1, 1, 1, backend_type)
-     << relu()
-     << fc(120, 15, true, backend_type)  // F6, 120-in, 15-out
-     << relu();
+    nn << conv(128, 128, 5, 1, 6,   // C1, 1@128x128-in, 6@124x124-out
+               padding::valid, true, 1, 1, 1, 1, backend_type)
+       << relu()
+       << ave_pool(28, 28, 6, 2)   // S2, 6@28x28-in, 6@14x14-out
+       << relu()
+       << conv(14, 14, 5, 6, 16,   // C3, 6@14x14-in, 16@10x10-out
+               connection_table(tbl, 6, 16),
+               padding::valid, true, 1, 1, 1, 1, backend_type)
+       << relu()
+       << ave_pool(10, 10, 16, 2)  // S4, 16@10x10-in, 16@5x5-out
+       << relu()
+       << conv(5, 5, 5, 16, 120,   // C5, 16@5x5-in, 120@1x1-out
+               padding::valid, true, 1, 1, 1, 1, backend_type)
+       << relu()
+       << fc(120, 15, true, backend_type)  // F6, 120-in, 15-out
+       << relu();
 }
 
 static void train_lenet(const std::string &data_dir_path,
                         double learning_rate,
                         const int n_train_epochs,
                         const int n_minibatch,
-                        tiny_dnn::core::backend_t backend_type) {
-  // specify loss-function and learning strategy
-  tiny_dnn::network<tiny_dnn::sequential> nn;
-  tiny_dnn::adagrad optimizer;
+                        tiny_dnn::core::backend_t backend_type)
+{
+    // specify loss-function and learning strategy
+    tiny_dnn::network<tiny_dnn::sequential> nn;
+    tiny_dnn::adagrad optimizer;
 
-  construct_net(nn, backend_type);
+    construct_net(nn, backend_type);
 
-  std::cout << "load models..." << std::endl;
+    std::cout << "load models..." << std::endl;
 
-  // load MNIST dataset
-  std::vector<tiny_dnn::vec_t> train_labels, test_labels;
-  std::vector<tiny_dnn::vec_t> train_images, test_images;
+    // load MNIST dataset
+    std::vector<tiny_dnn::vec_t> train_labels, test_labels;
+    std::vector<tiny_dnn::vec_t> train_images, test_images;
 
-  /*
-  tiny_dnn::parse_mnist_labels(data_dir_path + "/train-labels.idx1-ubyte",
+    /*
+    tiny_dnn::parse_mnist_labels(data_dir_path + "/train-labels.idx1-ubyte",
                                &train_labels);
-  tiny_dnn::parse_mnist_images(data_dir_path + "/train-images.idx3-ubyte",
+    tiny_dnn::parse_mnist_images(data_dir_path + "/train-images.idx3-ubyte",
                                &train_images, -1.0, 1.0, 2, 2);
-  tiny_dnn::parse_mnist_labels(data_dir_path + "/t10k-labels.idx1-ubyte",
+    tiny_dnn::parse_mnist_labels(data_dir_path + "/t10k-labels.idx1-ubyte",
                                &test_labels);
-  tiny_dnn::parse_mnist_images(data_dir_path + "/t10k-images.idx3-ubyte",
+    tiny_dnn::parse_mnist_images(data_dir_path + "/t10k-images.idx3-ubyte",
                                &test_images, -1.0, 1.0, 2, 2);
-  */
+    */
 
-  // load training images and labels
-  // load testing images and labels
+    // load training images and labels
+    // load testing images and labels
 
-  std::cout << "start training" << std::endl;
+    std::cout << "start training" << std::endl;
 
-  tiny_dnn::progress_display disp(train_images.size());
-  tiny_dnn::timer t;
+    tiny_dnn::progress_display disp(train_images.size());
+    tiny_dnn::timer t;
 
-  optimizer.alpha *=
+    optimizer.alpha *=
     std::min(tiny_dnn::float_t(4),
              static_cast<tiny_dnn::float_t>(sqrt(n_minibatch) * learning_rate));
 
-  int epoch = 1;
-  // create callback
-  /*
-  auto on_enumerate_epoch = [&]() {
-    std::cout << "Epoch " << epoch << "/" << n_train_epochs << " finished. "
-              << t.elapsed() << "s elapsed." << std::endl;
-    ++epoch;
-    tiny_dnn::result res = nn.test(test_images, test_labels);
-    std::cout << res.num_success << "/" << res.num_total << std::endl;
+    int epoch = 1;
+    // create callback
 
-    disp.restart(train_images.size());
-    t.restart();
-  };
+    auto on_enumerate_epoch = [&]() {
+        std::cout << "Epoch " << epoch << "/" << n_train_epochs << " finished. "
+                  << t.elapsed() << "s elapsed." << std::endl;
+        ++epoch;
 
-  auto on_enumerate_minibatch = [&]() { disp += n_minibatch; };
+        // show loss (can't figure out how to show accuracy)
+        std::cout << "Loss: "
+                  << nn.get_loss<tiny_dnn::mse>(test_images, test_labels)
+                  << std::endl;
 
-  // training (using fit for vec_t classification instead of train with label_t)
-  nn.fit<tiny_dnn::mse>(optimizer, train_images, train_labels, n_minibatch,
+        disp.restart(train_images.size());
+        t.restart();
+    };
+
+    auto on_enumerate_minibatch = [&]() { disp += n_minibatch; };
+
+    // train (using fit for vec_t classification opposed tovtrain for label_t)
+    nn.fit<tiny_dnn::mse>(optimizer, train_images, train_labels, n_minibatch,
                           n_train_epochs, on_enumerate_minibatch,
                           on_enumerate_epoch);
 
-  std::cout << "end training." << std::endl;
-  */
-  // test and show results
-  //nn.test(test_images, test_labels).print_detail(std::cout);
-  // save network model & trained weights
-  nn.save("xray-model");
-}
+    std::cout << "end training." << std::endl;
 
-static tiny_dnn::core::backend_t parse_backend_name(const std::string &name) {
-  const std::array<const std::string, 5> names = {{
-    "internal", "nnpack", "libdnn", "avx", "opencl",
-  }};
-  for (size_t i = 0; i < names.size(); ++i) {
-    if (name.compare(names[i]) == 0) {
-      return static_cast<tiny_dnn::core::backend_t>(i);
-    }
-  }
-  return tiny_dnn::core::default_engine();
-}
-
-static void usage(const char *argv0) {
-  std::cout << "Usage: " << argv0 << " --data_path path_to_dataset_folder"
-            << " --learning_rate 1"
-            << " --epochs 30"
-            << " --minibatch_size 16"
-            << " --backend_type internal" << std::endl;
-}
-
-int main(int argc, char **argv) {
-  double learning_rate                   = 1;
-  int epochs                             = 30;
-  std::string data_path                  = "";
-  int minibatch_size                     = 16;
-  tiny_dnn::core::backend_t backend_type = tiny_dnn::core::default_engine();
-
-  if (argc == 2) {
-    std::string argname(argv[1]);
-    if (argname == "--help" || argname == "-h") {
-      usage(argv[0]);
-      return 0;
-    }
-  }
-  for (int count = 1; count + 1 < argc; count += 2) {
-    std::string argname(argv[count]);
-    if (argname == "--learning_rate") {
-      learning_rate = atof(argv[count + 1]);
-    } else if (argname == "--epochs") {
-      epochs = atoi(argv[count + 1]);
-    } else if (argname == "--minibatch_size") {
-      minibatch_size = atoi(argv[count + 1]);
-    } else if (argname == "--backend_type") {
-      backend_type = parse_backend_name(argv[count + 1]);
-    } else if (argname == "--data_path") {
-      data_path = std::string(argv[count + 1]);
-    } else {
-      std::cerr << "Invalid parameter specified - \"" << argname << "\""
-                << std::endl;
-      usage(argv[0]);
-      return -1;
-    }
-  }
-  if (data_path == "") {
-    std::cerr << "Data path not specified." << std::endl;
-    usage(argv[0]);
-    return -1;
-  }
-  if (learning_rate <= 0) {
-    std::cerr
-      << "Invalid learning rate. The learning rate must be greater than 0."
-      << std::endl;
-    return -1;
-  }
-  if (epochs <= 0) {
-    std::cerr << "Invalid number of epochs. The number of epochs must be "
-                 "greater than 0."
+    // test and show resulting loss (can't figure out how to show accuracy)
+    std::cout << "Loss: "
+              << nn.get_loss<tiny_dnn::mse>(test_images, test_labels)
               << std::endl;
+    // save network model & trained weights
+    nn.save("xray-diagnosis-model");
+}
+
+static tiny_dnn::core::backend_t parse_backend_name(const std::string &name)
+{
+    const std::array<const std::string, 5> names = {{
+    "internal", "nnpack", "libdnn", "avx", "opencl",
+    }};
+    
+    for (size_t i = 0; i < names.size(); ++i) {
+        if (name.compare(names[i]) == 0) {
+            return static_cast<tiny_dnn::core::backend_t>(i);
+        }
+    }
+    
+    return tiny_dnn::core::default_engine();
+}
+
+static void usage(const char *argv0)
+{
+    std::cout << "Usage: " << argv0 << " --data_path path_to_dataset_folder"
+              << " --learning_rate 1"
+              << " --epochs 30"
+              << " --minibatch_size 16"
+              << " --backend_type internal" << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    double learning_rate                   = 1;
+    int epochs                             = 30;
+    std::string data_path                  = "";
+    int minibatch_size                     = 16;
+    tiny_dnn::core::backend_t backend_type = tiny_dnn::core::default_engine();
+
+    if (argc == 2) {
+        std::string argname(argv[1]);
+        if (argname == "--help" || argname == "-h") {
+            usage(argv[0]);
+            return 0;
+        }
+    }
+    for (int count = 1; count + 1 < argc; count += 2) {
+        std::string argname(argv[count]);
+        if (argname == "--learning_rate") {
+            learning_rate = atof(argv[count + 1]);
+        } else if (argname == "--epochs") {
+            epochs = atoi(argv[count + 1]);
+        } else if (argname == "--minibatch_size") {
+            minibatch_size = atoi(argv[count + 1]);
+        } else if (argname == "--backend_type") {
+            backend_type = parse_backend_name(argv[count + 1]);
+        } else if (argname == "--data_path") {
+            data_path = std::string(argv[count + 1]);
+        } else {
+            std::cerr << "Invalid parameter specified - \"" << argname << "\""
+                      << std::endl;
+            usage(argv[0]);
+            return -1;
+        }
+    }
+    if (data_path == "") {
+        std::cerr << "Data path not specified." << std::endl;
+        usage(argv[0]);
+        return -1;
+    }
+    if (learning_rate <= 0) {
+        std::cerr
+        << "Invalid learning rate. The learning rate must be greater than 0."
+        << std::endl;
+        return -1;
+    }
+    if (epochs <= 0) {
+        std::cerr << "Invalid number of epochs. The number of epochs must be "
+                     "greater than 0."
+                  << std::endl;
+        return -1;
+    }
+    if (minibatch_size <= 0 || minibatch_size > 60000) {
+        std::cerr
+        << "Invalid minibatch size. The minibatch size must be greater than 0"
+           " and less than dataset size (60000)."
+        << std::endl;
     return -1;
-  }
-  if (minibatch_size <= 0 || minibatch_size > 60000) {
-    std::cerr
-      << "Invalid minibatch size. The minibatch size must be greater than 0"
-         " and less than dataset size (60000)."
-      << std::endl;
-    return -1;
-  }
-  std::cout << "Running with the following parameters:" << std::endl
-            << "Data path: " << data_path << std::endl
-            << "Learning rate: " << learning_rate << std::endl
-            << "Minibatch size: " << minibatch_size << std::endl
-            << "Number of epochs: " << epochs << std::endl
-            << "Backend type: " << backend_type << std::endl
-            << std::endl;
-  try {
-    train_lenet(data_path, learning_rate, epochs, minibatch_size, backend_type);
-  } catch (tiny_dnn::nn_error &err) {
-    std::cerr << "Exception: " << err.what() << std::endl;
-  }
-  return 0;
+    }
+    std::cout << "Running with the following parameters:" << std::endl
+              << "Data path: " << data_path << std::endl
+              << "Learning rate: " << learning_rate << std::endl
+              << "Minibatch size: " << minibatch_size << std::endl
+              << "Number of epochs: " << epochs << std::endl
+              << "Backend type: " << backend_type << std::endl
+              << std::endl;
+    try {
+        train_lenet(data_path, learning_rate, epochs,
+                    minibatch_size, backend_type);
+    } catch (tiny_dnn::nn_error &err) {
+        std::cerr << "Exception: " << err.what() << std::endl;
+    }
+    return 0;
 }
