159574f36d08e88391818c75c428cd90b9c3af0c
skyedv <sdevelasco@ucdavis.edu>
2019-05-13 09:10:04 -0700

    updated bus for top control

--- hardware/synthesis/matrixMultiplier.v
+++ hardware/synthesis/matrixMultiplier.v
@@ -52,52 +52,60 @@ module matrixMultiplier (
     // ========================================================================
     // TPU
     // ========================================================================
-    wire wr_en_weights = slave_write & (slave_address[9:8] == `WEIGHT_OFFSET;
-    wire wr_en_input = slave_write & slave_address[9:8] == `INPUT_OFFSET;
-    wire rd_en_weights = slave_read & (slave_address[9:8] == `WEIGHT_OFFSET;
-    wire rd_en_input = slave_read & (slave_address[9:8] == `INPUT_OFFSET;
+    wire wr_en_weight = slave_write & (slave_address[9:8] == `WEIGHT_OFFSET;
+    wire wr_en_data = slave_write & slave_address[9:8] == `INPUT_OFFSET;
+    //wire rd_en_weights = slave_read & (slave_address[9:8] == `WEIGHT_OFFSET;
+    //wire rd_en_input = slave_read & (slave_address[9:8] == `INPUT_OFFSET;
+    //
+    wire wr_addr
 
-    reg ld_fifo;
-    reg ld_weights;
+    reg wr_en_fifo;
+    reg load_en_weight;
     reg mult_en;
-    
-    wire rd_data_weights;
-    wire rd_data_input;
 
-    top_tpu top(
-        .wr_en_weights(wr_en_weights),
-        .wr_en_input(wr_en_input),
-        .rd_en_weights(rd_en_weights),
-        .rd_en_input(rd_en_input),
-        .wr_addr_weights(slave_address[7:0]),
-        .wr_addr_input(slave_address[7:0]),
-        .rd_addr_weights(slave_address[7:0]),
-        .rd_addr_input(slave_address[7:0]),
-        .base_addr_weights(slave_address[7:0]),
-        .base_addr_input(slave_address[7:0]),
-        .ld_fifo(ld_fifo),
-        .ld_weights(ld_weights),
-        .mult_en(mult_en),
-        .rd_data_weights(rd_data_weights),
-        .rd_data_input(rd_data_input)
+    
+    //wire rd_data_weights;
+    //wire rd_data_input;
+
+    top_TPU top(    //no reads???
+        .clk(clk),
+        .reset(reset),
+        .wr_en_weight(wr_en_weights),           //enable write weights to mem
+        .wr_en_data(wr_en_input),               //enable write data/input to mem
+        .wr_en_fifo(wr_en_fifo),                //enable write from mem to fifo
+        //.rd_en_weights(rd_en_weights),
+        //.rd_en_input(rd_en_input),
+        .wr_addr_weight(slave_address[7:0]),    //write weight addr to mem
+        //.wr_addr_input(slave_address[7:0]),
+        .wr_data_weight(slave_writedata),        //write weight data/input to mem
+        //.rd_addr_weights(slave_address[7:0]),
+        //.rd_addr_input(slave_address[7:0]),
+        .base_addr_weight(slave_address[7:0]),  //base addr for weights in mem
+        .base_addr_data(slave_address[7:0]),    //base addr for data/inputs in mem
+        //.load_fifo(ld_fifo),
+        .load_en_weight(load_en_weight),       //load weight from fifo & write to array?
+        .mult_en(mult_en),                      //enable the multiplication
+        //.rd_data_weights(rd_data_weights),
+        //.rd_data_input(rd_data_input),
+        .wr_addr_output(wr_addr_output)         //output addr to write to?
         //to-do: add outputs
     );
 
     always @ (*) begin
-        case(slave_address[9:8]) begin
+        case(slave_address[9:8]) begin //wtf do I do here now
             `CONTROL_OFFSET:
                 //slave_read_data = GET FROM ALAN
             `WEIGHT_OFFSET:
-                slave_read_data = rd_data_weights;
+                //slave_read_data = rd_data_weights;
             `INPUT_OFFSET:
-                slave_read_data = rd_data_input;
+                //slave_read_data = rd_data_input;
         endcase
     end
 
     always @ (posedge clk) begin
         if (slave_write == 1 & slave_address[9:8] == `CONTROL_OFFSET) begin
-            ld_fifo <= slave_address[2];
-            ld_weights <= slave_address[1];
+            wr_en_fifo <= slave_address[2];
+            load_en_weights <= slave_address[1];
             mult_en <= slave_address[0];
         end
     end
