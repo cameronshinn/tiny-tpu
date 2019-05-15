d06c4edc7d5f18bfef2ba3667df020399dd8b720
SkyrimYu <30339416+SkyrimYu@users.noreply.github.com>
2019-05-15 12:08:50 -0700

    Update tb_wr_mem.v

--- hardware/testbenches/tb_wr_mem.v
+++ hardware/testbenches/tb_wr_mem.v
@@ -1,9 +1,12 @@
 module tb_wr_mem();
 
-reg clk;
-reg [15:0] wr_en, rd_en;
-reg [127:0] wr_addr, wr_data, rd_addr;
+reg clk, reset, rd_active, wr_active;
+wire [15:0] wr_en, rd_en;
+wire [127:0] wr_addr, rd_addr;
+reg [127:0] wr_addr_mem, rd_addr_mem;
+reg [127:0] wr_data;
 wire [127:0] rd_data;
+reg [15:0] wr_en_mem, rd_en_mem;
 
 integer i, j;
 
@@ -14,30 +17,47 @@ end
 
 initial begin
   clk = 0;
-  wr_en = 0;
-  wr_addr = 0;
+  reset = 1;
   wr_data = 0;
-  rd_en = 0;
-  rd_addr = 0;
   #10;
-  for(i=0; i<128; i=i+1) begin
-    wr_en = wr_en << 1 + 1;
+  for(i = 0; i < 16; i = i + 1) begin
+    wr_active = 1;
     wr_data = wr_data << 8 + i;
-    wr_addr = wr_addr << 8 + i;
     #10;
   end
   #10;
-  for(j=0; j<128; j=j+1) begin
-    rd_en = rd_en << 1 + 1;
-    rd_addr = rd_addr << 8 + 1;
-    $display(rd_data );
+  for(j = 0; j < 16; j = j + 1) begin
+    rd_active = 1;
+    $display(rd_data);
     #10;
   end
-  #10;
-  $stop;
 end
 
-wr_mem_control CONTROL (.clk(clk), .wr_en(wr_en), .wr_addr(wr_addr), .wr_data(wr_data),
-    .rd_en(rd_en), .rd_addr(rd_addr), .rd_data(rd_data));
+rd_control RD(
+  .clk(clk), // clock signal
+  .reset(reset), // reset the inputs and reg signals
+  .active(rd_active), // this module only works when the active is high
+  .rd_en(rd_en), // enable accessing to the memeory
+  .rd_addr(rd_addr) // read address the full address should be base addr + wr_addr
+  );
+
+wr_control WR(
+  .clk(clk), // clock signal
+  .reset(reset), // reset the inputs and reg signals
+  .active(wr_active), // this module only works when the active is high
+  .wr_en(wr_en), // enable accessing to the memeory
+  .wr_addr(wr_addr) // write address (offset), the full address should be base addr + wr_addr
+  );
+
+memArr MEM(
+   .clk(clk),
+   .rd_en(rd_en),
+   .wr_en(wr_en),
+   .wr_data(wr_data),
+   .rd_data(rd_data),
+   .wr_addr(wr_addr),
+   .rd_addr(rd_addr)
+);
+defparam MEM.width_height = 16;
 
 endmodule
