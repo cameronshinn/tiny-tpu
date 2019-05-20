1179ae6236ae4f4fe0a5a1ab9667c5af2904ba42
SkyrimYu <30339416+SkyrimYu@users.noreply.github.com>
2019-05-20 13:33:00 -0700

    Update rd_control.v

--- hardware/hdl/control/rd_control.v
+++ hardware/hdl/control/rd_control.v
@@ -8,27 +8,36 @@ module rd_control(
   reset, // reset the inputs and reg signals
   active, // this module only works when the active is high
   rd_en, // enable accessing to the memeory
-  rd_addr // read address the full address should be base addr + wr_addr
+  rd_addr, // read address the full address should be base addr + wr_addr
+  wr_active // enable write output control
   );
 
   parameter width_height = 16;
   localparam data_width = 8 * width_height; // number of data bits needed
+  localparam  count_width = $clog2(width_height * 2);
 
   input clk, reset, active;
   output reg [width_height-1:0] rd_en;
   output reg [data_width-1:0] rd_addr;
+  output reg wr_active;
 
   reg [width_height-1:0] rd_en_c;
   reg [data_width-1:0] rd_addr_c, rd_inc;
-  reg rd_dec;
+  reg [count_width-1:0] count, count_c;
+  reg rd_dec, rd_start;
 
   always@(posedge clk) begin
     rd_en <= rd_en_c;
     rd_addr <= rd_addr_c;
+    count <= count_c;
   end
 
   always@(*) begin
-    if(active) begin // start to get read address
+    if(active) begin
+      rd_start = 1;
+    end
+
+    if(rd_start) begin // start to get read address
       if(rd_en == 16'hffff) begin
         rd_dec = 1;
       end
@@ -43,6 +52,12 @@ module rd_control(
 
       rd_inc = {15'b0, rd_en[3], 15'b0, rd_en[2], 15'b0, rd_en[1], 15'b0, rd_en[0]};
       rd_addr_c = rd_inc + rd_addr;
+
+      count_c = count + 1;
+
+      if(count >= 18) begin
+        wr_active = 1;
+      end
     end
 
     else begin
@@ -53,6 +68,9 @@ module rd_control(
       rd_addr_c = 0;
       rd_en_c  = 16'h0000;
       rd_dec = 0;
+      rd_start = 0;
+      count_c = 0;
+      wr_active = 0;
     end
   end
 endmodule
