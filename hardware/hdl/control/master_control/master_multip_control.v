6019fe90c3013e3139968d9d191f1f96ecf8d844
cameronshinn <cameron.shinn@hotmail.com>
2019-05-22 03:52:56 -0700

    Renamed, removed and added input signals

--- hardware/hdl/control/master_control/master_multip_control.v
+++ hardware/hdl/control/master_control/master_multip_control.v
@@ -2,8 +2,9 @@ module master_multip_control(
   clk,
   reset,
   active,
-  num_row,
-  num_col,
+  intermed_dim,
+  num_row_weight_mat,
+  num_col_in_mat,
   base_data, // base addr of the data
   accum_table_submat_row,
   accum_table_submat_col,
@@ -27,7 +28,7 @@ parameter D_mem_calc = 2'b11;
 
 input clk, reset, active;
 input weight_mem_fifo_done, weight_fifo_arr_done, data_mem_calc_done;
-input [$clog2(width_height)-1:0] num_row, num_col;
+input [$clog2(width_height)-1:0] num_row_weight_mat, num_col_in_mat, intermed_dim;
 input [data_width-1:0] base_data;
 input [$clog2(MAX_OUT_COLS/SYS_ARR_WIDTH)-1:0] accum_table_submat_col; 
 input [$clog2(MAX_OUT_ROWS/SYS_ARR_HEIGHT)-1:0] accum_table_submat_row;
