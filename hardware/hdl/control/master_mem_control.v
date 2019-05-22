6e6ba9e993cb0fb4b15473745d80f9626ffd76c4
SkyrimYu <30339416+SkyrimYu@users.noreply.github.com>
2019-05-21 20:30:12 -0700

    Update master_mem_control.v

--- hardware/hdl/control/master_mem_control.v
+++ hardware/hdl/control/master_mem_control.v
@@ -6,6 +6,7 @@ module master_mem_control(
   num_row,
   num_col,
   out_addr,
+  out_en,
   done
 );
 
@@ -18,6 +19,7 @@ input [addr_width-1:0] base_addr;
 input [$clog2(width_height)-1:0] num_row, num_col;
 
 output reg done;
+output reg [width_height-1:0] out_en;
 output reg [out_addr_width-1:0] out_addr;
 
 reg start;
@@ -34,12 +36,14 @@ always@(*) begin
   end
 
   if(start) begin
+    out_en = {width_height{1'b1}} >> (width_height - num_col + 1);
     out_addr = {width_height{base_addr+count}};
     count_c = count + 1;
 
     if(count >= num_row + 1) begin
       count_c = 0;
       out_addr = 0;
+      out_en = 0;
       done = 1;
       start = 0;
     end
