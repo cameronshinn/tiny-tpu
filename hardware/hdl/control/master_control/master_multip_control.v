fde89eb1bbce22bc432e67ddfd2dac50819724f9
cameronshinn <cameron.shinn@hotmail.com>
2019-05-22 07:39:20 -0700

    Fixed parameters

--- hardware/hdl/control/master_control/master_multip_control.v
+++ hardware/hdl/control/master_control/master_multip_control.v
@@ -20,8 +20,7 @@ module master_multip_control(
 
 parameter width_height = 16;
 parameter data_width = width_height * 8;
-parameter MAX_OUT_ROWS = 128;
-parameter MAX_OUT_COLS = 128;
+parameter max_out_width_height = 128;
 
 parameter Hold = 2'b00;
 parameter W_mem_fifo = 2'b01;
@@ -32,11 +31,11 @@ input clk, reset, active;
 input weight_fifo_arr_done, data_mem_calc_done;
 input [$clog2(width_height)-1:0] num_row_weight_mat, num_col_in_mat, intermed_dim;
 input [data_width-1:0] base_data;
-input [$clog2(MAX_OUT_COLS/SYS_ARR_WIDTH)-1:0] accum_table_submat_col_in; 
-input [$clog2(MAX_OUT_ROWS/SYS_ARR_HEIGHT)-1:0] accum_table_submat_row_in;
+input [$clog2(max_out_width_height/width_height)-1:0] accum_table_submat_col_in; 
+input [$clog2(max_out_width_height/width_height)-1:0] accum_table_submat_row_in;
 
-output wire [$clog2(MAX_OUT_COLS/SYS_ARR_WIDTH)-1:0] accum_table_submat_col_out;
-output wire [$clog2(MAX_OUT_ROWS/SYS_ARR_HEIGHT)-1:0] accum_table_submat_row_out;
+output wire [$clog2(max_out_width_height/width_height)-1:0] accum_table_submat_col_out;
+output wire [$clog2(max_out_width_height/width_height)-1:0] accum_table_submat_row_out;
 output reg weight_fifo_arr_en, data_mem_calc_en;
 output wire fifo_ready; //indicates when the fill_fifo instruction can be called
 // ^^ need to latch some of the inputs when this goes high so that new data can
