6b9b2d81b7b32d2fd2ac329f0cb6668cfeda6052
cameronshinn <cameron.shinn@hotmail.com>
2019-05-29 12:58:20 -0700

    Formatted comment

--- hardware/hdl/control/master_control/master_control.v
+++ hardware/hdl/control/master_control/master_control.v
@@ -1,73 +1,73 @@
 // master_control.v
 // Cameron Shinn
 
-/*
-The master controller takes instructions from the bus as its input and drives
-the corresponding control signals to the individual controllers of each
-submodule
-
-Instruction set:
-*signals marked with NONE are ignored*
-
-read_inputs() - Read inputs from the bus into input memory
-    @opcode: 001 (1)
-    @dim_1: number of rows in matrix
-    @dim_2: number of columns in matrix
-    @dim_3: NONE
-    @addr_1: base address to store in input memory
-    @accum_table_submat_row_in: NONE
-    @accum_table_submat_col_in: NONE
-
-read_weights() - Read weights from the bus into weight memory
-    @opcode: 010 (2)
-    @dim_1: number of rows in weight matrix
-    @dim_2: number of columns in weight matrix
-    @dim_3: NONE
-    @addr_1: base address to store in weight memory
-    @accum_table_submat_row_in: NONE
-    @accum_table_submat_col_in: NONE
-
-fill_fifo() - Fill the FIFO with weights from the give memory location
-    @opcode: 011 (3)
-    @dim_1: number of rows in weight matrix
-    @dim_2: number of columns in weight matrix
-    @dim_3: NONE
-    @addr_1: base address to read from weight memory
-    @accum_table_submat_row_in: NONE
-    @accum_table_submat_col_in: NONE
-
-matrix_multiply() - Empty the FIFO into the systolic array, perform a matrix multiply, and store outputs in the accumulator table
-    @opcode: 100 (4)
-    @dim_1: intermediate dimension (number of columns in weight matrix and number of rows in input matrix)
-    @dim_2: number of rows in weight matrix
-    @dim_3: number of columns in input matrix
-    @addr_1: base address of input matrix in the input memory
-    @accum_table_submat_row_in: row index of the accumulator table where the output matrix will be stored
-    @accum_table_submat_col_in: column index of the accumulator table where the output matrix will be stored
-
-store_outputs()
-    @opcode: 101 (5)
-    @dim_1: number of rows to read from accumulator table index
-    @dim_2: number of columns to read from accumulator table index
-    @dim_3: LSB set to 1 to perform ReLU activation on the outputs or 0 to store them as-is
-            2nd LSB set to 1 to clear the entire accumulator table after the read or 0 otherwise
-    @addr_1: base address to write to output memory
-    @accum_table_submat_row_in: row index of the accumulator table where the output matrix will be read from
-    @accum_table_submat_col_in: column index of the accumulator table where the output matrix will be read from
-
-write_outputs()
-    @opcode: 110 (6)
-    @dim_1: number of rows in output matrix
-    @dim_2: number of columns in output matrix
-    @dim_3: NONE
-    @addr_1: base address to read from output memory
-    @accum_table_submat_row_in: NONE
-    @accum_table_submat_col_in: NONE
-
-init_tpu() - Resets the TPU
-    @opcode 111 (7)
-    all else: NONE
-*/
+/**
+ * The master controller takes instructions from the bus as its input and drives
+ * the corresponding control signals to the individual controllers of each
+ * submodule
+ * 
+ * Instruction set:
+ * signals marked with NONE are ignored
+ * 
+ * read_inputs() - Read inputs from the bus into input memory
+ *     @opcode: 001 (1)
+ *     @dim_1: number of rows in matrix
+ *     @dim_2: number of columns in matrix
+ *     @dim_3: NONE
+ *     @addr_1: base address to store in input memory
+ *     @accum_table_submat_row_in: NONE
+ *     @accum_table_submat_col_in: NONE
+ * 
+ * read_weights() - Read weights from the bus into weight memory
+ *     @opcode: 010 (2)
+ *     @dim_1: number of rows in weight matrix
+ *     @dim_2: number of columns in weight matrix
+ *     @dim_3: NONE
+ *     @addr_1: base address to store in weight memory
+ *     @accum_table_submat_row_in: NONE
+ *     @accum_table_submat_col_in: NONE
+ * 
+ * fill_fifo() - Fill the FIFO with weights from the give memory location
+ *     @opcode: 011 (3)
+ *     @dim_1: number of rows in weight matrix
+ *     @dim_2: number of columns in weight matrix
+ *     @dim_3: NONE
+ *     @addr_1: base address to read from weight memory
+ *     @accum_table_submat_row_in: NONE
+ *     @accum_table_submat_col_in: NONE
+ * 
+ * matrix_multiply() - Empty the FIFO into the systolic array, perform a matrix multiply, and store outputs in the accumulator table
+ *     @opcode: 100 (4)
+ *     @dim_1: intermediate dimension (number of columns in weight matrix and number of rows in input matrix)
+ *     @dim_2: number of rows in weight matrix
+ *     @dim_3: number of columns in input matrix
+ *     @addr_1: base address of input matrix in the input memory
+ *     @accum_table_submat_row_in: row index of the accumulator table where the output matrix will be stored
+ *     @accum_table_submat_col_in: column index of the accumulator table where the output matrix will be stored
+ * 
+ * store_outputs()
+ *     @opcode: 101 (5)
+ *     @dim_1: number of rows to read from accumulator table index
+ *     @dim_2: number of columns to read from accumulator table index
+ *     @dim_3: LSB set to 1 to perform ReLU activation on the outputs or 0 to store them as-is
+ *             2nd LSB set to 1 to clear the entire accumulator table after the read or 0 otherwise
+ *     @addr_1: base address to write to output memory
+ *     @accum_table_submat_row_in: row index of the accumulator table where the output matrix will be read from
+ *     @accum_table_submat_col_in: column index of the accumulator table where the output matrix will be read from
+ * 
+ * write_outputs()
+ *     @opcode: 110 (6)
+ *     @dim_1: number of rows in output matrix
+ *     @dim_2: number of columns in output matrix
+ *     @dim_3: NONE
+ *     @addr_1: base address to read from output memory
+ *     @accum_table_submat_row_in: NONE
+ *     @accum_table_submat_col_in: NONE
+ * 
+ * init_tpu() - Resets the TPU
+ *     @opcode 111 (7)
+ *     all else: NONE
+ */
 
 module master_control(clk,
                       reset,
