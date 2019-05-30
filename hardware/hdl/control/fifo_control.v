5be01a5dabb3105ad6366196c1f1af1d1f3a755b
Wesly Tonks <watonks@ucdavis.edu>
2019-05-30 15:42:02 -0700

    Fix some compile time warnings/errors in fifo_control.v

--- hardware/hdl/control/fifo_control.v
+++ hardware/hdl/control/fifo_control.v
@@ -11,22 +11,22 @@ module fifo_control(
     weight_write
     );
 
-    parameter WIDTH_HEIGHT = 16;
-    localparam COUNT_WIDTH = $clog2(WIDTH_HEIGHT) + 1;
+    parameter fifo_width = 16;
+    localparam COUNT_WIDTH = $clog2(fifo_width) + 1;
 
     input clk;
     input reset;
     input active;
     input stagger_load;
-    output wire [WIDTH_HEIGHT-1:0] fifo_en;
+    output wire [fifo_width-1:0] fifo_en;
     output wire done;
     output wire weight_write;
     
     reg started, started_c;
-    reg count, count_c;
+    reg [COUNT_WIDTH - 1:0] count, count_c;
     reg stagger_latch, stagger_latch_c; // must latch to prevent changing midway
 
-    assign fifo_en = (stagger_latch) ? ({WIDTH_HEIGHT{1'b1}}) : {WIDTH_HEIGHT{1'b1}}; // FIXME: First case
+    assign fifo_en = (stagger_latch) ? ({fifo_width{1'b1}}) : {fifo_width{1'b1}}; // FIXME: First case
     assign done = ~started;
     assign weight_write = started;
 
@@ -45,22 +45,22 @@ module fifo_control(
             count_c = count + 1'b1;
 
             if (stagger_latch) begin
-                if (count == WIDTH_HEIGHT*2-1) begin
+                if (count == fifo_width*2-1) begin
                     started_c = 1'b0;
-                end // if (count == WIDTH_HEIGHT*2-1)
+                end // if (count == fifo_width*2-1)
             end // if (stagger_latch)
 
             else begin // not staggered load
-                if (count == WIDTH_HEIGHT-1) begin
+                if (count == fifo_width-1) begin
                     started_c = 1'b0;
-                end // if (count == WIDTH_HEIGHT-1)
+                end // if (count == fifo_width-1)
             end // else
         end // if (started)
 
         if (reset) begin
             started_c = 1'b0;
             count_c = {COUNT_WIDTH{1'b0}};
-            stagger_latch_c = stagger_load
+            stagger_latch_c = stagger_load;
         end // if (reset)
     end // always @(*)
 
