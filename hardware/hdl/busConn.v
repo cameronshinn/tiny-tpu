1d1577683821495987a82179c4942ca6b543fb5c
Swasly <watonks@ucdavis.edu>
2019-06-01 14:47:50 -0700

    Add default case for when we're not writing to control.

--- hardware/hdl/busConn.v
+++ hardware/hdl/busConn.v
@@ -167,6 +167,15 @@ module matrixMultiplier (
                 end
             endcase // slave_writedata[3:0]
         end // if ((slave_write == 1) && (slave_address[9:8] == `CONTROL_OFFSET))
+        else begin
+            reset_tpu <= 1'b0;
+            fill_fifo <= 1'b0;
+            drain_fifo <= 1'b0;
+            multiply <= 1'b0;
+            weightMem_rd_addr_base <= weightMem_rd_addr_base;
+            inputMem_rd_addr_base <= inputMem_rd_addr_base;
+            outputMem_wr_addr_base <= outputMem_wr_addr_base;
+        end
     end // always @(posedge clk)
 
 
